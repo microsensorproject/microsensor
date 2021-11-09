0e0e67ac6142d1062422692f239c5aefaabebbfe
edson-a-soares <edson.araujo.soares@gmail.com>
2021-11-09 08:23:22 -0300

    Add analysis output (e.g., report) processing

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -4,5 +4,6 @@ TARGET_DIRECTORY="${1:-"/home/runner/work/_temp/_github_workflow"}"
 REQUEST_JSON_PAYLOAD="{ \"pathToCompiledMicroservices\": \"$TARGET_DIRECTORY\", \"organizationPath\": \"\", \"outputPath\": \"\" }"
 
 source /usr/local/msa-nose/msanose.sh > /dev/null
-curl -X POST http://127.0.0.1:8080/api/v1/report -H 'Content-Type: application/json' -d "$REQUEST_JSON_PAYLOAD"
-echo -e "\n"
+REPORT=$(curl -X POST http://127.0.0.1:8080/api/v1/report -H 'Content-Type: application/json' -d "$REQUEST_JSON_PAYLOAD")
+
+echo "::set-output name=report::$REPORT"
