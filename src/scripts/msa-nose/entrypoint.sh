7645095383f28ff96c5977dca2c024e0c68a840a
edson-a-soares <edson.araujo.soares@gmail.com>
2021-12-02 18:55:30 -0300

    Export json_report as an action output param

--- src/scripts/msa-nose/entrypoint.sh
+++ src/scripts/msa-nose/entrypoint.sh
@@ -5,5 +5,5 @@ source /usr/local/msa-nose/start_msanose.sh > /dev/null
 TARGET_DIRECTORY="${1:-"/home/runner/work/_temp/_github_workflow"}"
 REQUEST_JSON_PAYLOAD="{ \"pathToCompiledMicroservices\": \"$TARGET_DIRECTORY\", \"organizationPath\": \"\", \"outputPath\": \"\" }"
 
-REPORT=$(curl -X POST http://127.0.0.1:8080/api/v1/report -H 'Content-Type: application/json' -d "$REQUEST_JSON_PAYLOAD")
-echo "::set-output name=report::$REPORT"
+JSON_REPORT=$(curl -X POST http://127.0.0.1:8080/api/v1/report -H 'Content-Type: application/json' -d "$REQUEST_JSON_PAYLOAD")
+echo "::set-output name=json_report::$JSON_REPORT"
