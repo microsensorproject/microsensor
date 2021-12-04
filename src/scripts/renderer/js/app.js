69ef0c431dcd54f24b73c9213545d844b690c965
edson-a-soares <edson.araujo.soares@gmail.com>
2021-12-03 21:14:12 -0300

    Get JSON report available as Env variable into script

--- src/scripts/renderer/js/app.js
+++ src/scripts/renderer/js/app.js
@@ -6,6 +6,14 @@ const github = require('@actions/github')
 const templateSource = fs.readFileSync(__dirname + "/../templates/report.html", 'utf8')
 
 async function main() {
+
+    const INPUT_JSON_REPORT = core.getInput('INPUT_JSON_REPORT') || process.env.INPUT_JSON_REPORT
+    if ( typeof INPUT_JSON_REPORT !== 'string' ) {
+        throw new Error('Invalid INPUT_JSON_REPORT: did you forget to set INPUT_JSON_REPORT?')
+    }
+
+    console.log(INPUT_JSON_REPORT)
+
     // const jsonReportInput = JSON.parse(core.getInput("json_report"));
     // console.log(jsonReportInput)
 
