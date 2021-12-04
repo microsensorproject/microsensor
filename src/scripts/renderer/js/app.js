2a61dfe68d0712a025f05738362896f6f06b33e0
edson-a-soares <edson.araujo.soares@gmail.com>
2021-12-03 22:02:17 -0300

    Add basic dynamic HTML template report

--- src/scripts/renderer/js/app.js
+++ src/scripts/renderer/js/app.js
@@ -1,23 +1,22 @@
 const fs = require("fs")
 const core = require('@actions/core')
 const handlebars = require('handlebars')
-const github = require('@actions/github')
 
 const templateSource = fs.readFileSync(__dirname + "/../templates/report.html", 'utf8')
 
 async function main() {
-    const INPUT_JSON_REPORT = core.getInput('INPUT_JSON_REPORT') || process.env.INPUT_JSON_REPORT
+    let INPUT_JSON_REPORT = core.getInput('INPUT_JSON_REPORT') || process.env.INPUT_JSON_REPORT
     if ( typeof INPUT_JSON_REPORT !== 'string' ) {
         throw new Error('Invalid INPUT_JSON_REPORT: did you forget to set INPUT_JSON_REPORT?')
+        // INPUT_JSON_REPORT = fs.readFileSync(__dirname + "/../data/report-sample.json", 'utf8')
     }
 
     const jsonReportInput = JSON.parse(INPUT_JSON_REPORT);
-    console.log(jsonReportInput)
+    // console.log(INPUT_JSON_REPORT)
 
     const template = handlebars.compile(templateSource);
-    const data = { "title": "MSA Nose Report" };
-    const report = template(data)
-    console.log(report)
+    const report = template(jsonReportInput)
+    // console.log(report)
     core.setOutput("html_report", report)
 }
 
