e8bb1d058aed3350c1ce2718d2820b1c6f90cd6d
Edson A. Soares <edson.araujo.soares@gmail.com>
2022-06-19 19:51:16 -0300

    Config webpack with GitHub infrastructure

--- src/scripts/renderer/startup/webpack.config.js
+++ src/scripts/renderer/startup/webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const process = require('process')
+const core = require('@actions/core')
 const { merge } = require("webpack-merge");
 const CopyPlugin = require("copy-webpack-plugin");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+const OUTPUT_HTML_REPORT_DIRECTORY = core.getInput('OUTPUT_HTML_REPORT_DIRECTORY')
+	||  process.env.OUTPUT_HTML_REPORT_DIRECTORY
+	|| "dist";
+
+const distDir = path.join(process.env.GITHUB_WORKSPACE || path.join(__dirname, '../../../..'), OUTPUT_HTML_REPORT_DIRECTORY);
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "microsensor";
@@ -25,18 +34,17 @@ module.exports = (webpackConfigEnv, argv) => {
           orgName,
         },
       }),
-		new CopyPlugin({
-			patterns: [
-				{
-					from: path.join(__dirname, '../../../..', 'data'),
-					to: path.join(__dirname, '../../../..', 'dist')
-				},
-			],
+	  new CopyPlugin({
+		  patterns: [
+	        {
+		      from: path.join(__dirname, '../../../..', 'data'),
+			  to: distDir
+			},
+		  ],
 		}),
-
     ],
 	output : {
-		path: path.join(__dirname, '../../../..', 'dist')
+		path: distDir
 	}
   });
 };
