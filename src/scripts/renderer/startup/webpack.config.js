480c44450daf1b56b65ecc033642fadafb2692a2
Edson A. Soares <edson.araujo.soares@gmail.com>
2022-06-20 01:12:43 -0300

    Try different syntax for expression

--- src/scripts/renderer/startup/webpack.config.js
+++ src/scripts/renderer/startup/webpack.config.js
@@ -7,8 +7,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 
 
-const ORIGIN = core.getInput('GITHUB_PAGES_ORIGIN')
-	||  process.env.GITHUB_PAGES_ORIGIN
+const ORIGIN = core.getInput('GITHUB_REPOSITORY_OWNER').concat(".github.io/").concat(core.getInput('REPOSITORY_NAME'))
+	||  process.env.GITHUB_REPOSITORY_OWNER.concat(".github.io/").concat(process.env.REPOSITORY_NAME)
 	|| "http://localhost:9000";
 
 const OUTPUT_HTML_REPORT_DIRECTORY = core.getInput('OUTPUT_HTML_REPORT_DIRECTORY')
